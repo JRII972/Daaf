27ce14e6caf42078d1e210032db386e6302774e1
JRII972 <jeremyjovinac@hotmail.com>
2023-05-15 19:20:55 +0000

    Update prix marche

--- daaf/sisep/doctype/prix_et_marche/prix_et_marche.js
+++ daaf/sisep/doctype/prix_et_marche/prix_et_marche.js
@@ -6,3 +6,79 @@
 
 // 	},
 // });
+
+function calc_price(row){
+    console.log(row.unité)
+    frappe.db.get_doc('Unite', row.unité)
+    .then(unite => {
+        console.log(unite)
+        row.quantite_ref = row.quantité * unite.coef
+        row.prix_ref = row.prix_vente * unite.coef
+
+        if (unite.unit_ref) {
+            row.unite_ref = unite.unit_ref
+        } else {
+            row.unite_ref = row.unité
+        }
+
+        frm.refresh();
+    })
+}
+
+frappe.ui.form.on('Releve Mercuriale', {
+    // cdt is Child DocType name i.e Releve Mercuriale
+    // cdn is the row name for e.g bbfcb8da6a
+    prix_vente(frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn);
+        
+        if (row.prix_vente <= 0 ){
+            frappe.msgprint({
+                title: __('Notification'),
+                indicator: 'red',
+                message: __('le prix ne peux être négatif ou null')
+            });
+        } else {
+            if (row.quantité > 0){
+                
+                if ( row.unité ){
+                    calc_price(row)
+                }
+            }
+        }
+    },
+    Quantite_ref(frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn);
+        
+        if (row.quantité <= 0 ){
+            frappe.msgprint({
+                title: __('Notification'),
+                indicator: 'red',
+                message: __('la quantité ne peux être négatif ou null')
+            });
+        } else {
+            if (row.prix_vente > 0){
+                
+                if ( row.unité ){
+                    calc_price(row)
+                }
+            }
+        }
+    },
+    Quantite_ref(frm, cdt, cdn) {
+        let row = frappe.get_doc(cdt, cdn);
+        
+        if (row.quantité <= & row.prix_vente > 0){
+                
+                if ( row.unité ){
+                    calc_price(row)
+                } else {
+                    frappe.msgprint({
+                        title: __('Notification'),
+                        indicator: 'red',
+                        message: __('Unité obligatoire')
+                    });
+                }
+            }
+        }
+    }
+})
